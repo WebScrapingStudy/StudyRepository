d8ffe4554558d1d2e63dccd53541dd0c99eb3ea0
alsghdlah1 <alsghdlah1@naver.com>
2019-03-24 00:02:39 +0900

    강의9강~14강

--- 2주차/최민호/lecture_9to14.py
+++ 2주차/최민호/lecture_9to14.py
@@ -1,6 +1,9 @@
-import urllib.request
+'''9강'''
+import urllib.request               #urllib에 request한다.
 
 url = "https://www.naver.com/"
-html = urllib.request.urlopen(url)
+html = urllib.request.urlopen(url)  #urlopen이라는 function을 호출
 
-print(html.read())
+print(html.read())                  
+
+'''10강'''
