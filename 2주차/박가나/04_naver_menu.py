46ecb51d2adb354ae80c6938de56af66eaac83d4
gnchoco97 <48611443+gnchoco97@users.noreply.github.com>
2019-03-24 22:42:12 +0900

    #2 assignment

---
+++ 2주차/박가나/04_naver_menu.py
@@ -0,0 +1,16 @@
+import urllib.request
+import bs4
+
+url = "https://www.naver.com"
+html = urllib.request.urlopen(url)
+
+bsObj = bs4.BeautifulSoup(html, "html.parser")
+
+ul = bsObj.find("ul", {"class" : "an_l"})
+
+lis = ul.findAll("li")
+
+for li in lis :
+    a_tag = li.find("a")
+    span = a_tag.find("span",{"class" : "an_txt"})
+    print(span.text)
