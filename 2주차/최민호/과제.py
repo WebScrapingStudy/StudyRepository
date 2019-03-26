28d52176dca0d2423bdc51c436c40c629c00594d
alsghdlah1 <alsghdlah1@naver.com>
2019-03-26 10:09:20 +0900

    공공기관 채용 크롤링

--- 2주차/최민호/과제.py
+++ 2주차/최민호/과제.py
@@ -5,9 +5,10 @@ import urllib.request
 browser = webdriver.Chrome('C:/Users/Minho/Desktop/chromedriver.exe')
 url = 'http://www.alio.go.kr/informationRecruit.do'
 browser.get(url)
-#정규직, 채용 진행중만 출력하기
+#정규직, 채용 진행중, 대체인력 유무 N 만 출력하기
 browser.find_element_by_xpath('//*[@id="chk2_R1010"]').click()
 browser.find_element_by_xpath('//*[@id="chk3_Y"]').click()
+browser.find_element_by_xpath('//*[@id="replacement_N"]').click()
 
 #검색 버튼 누르기
 browser.find_element_by_xpath('//*[@class="cbot_area"]/a').click()
@@ -15,8 +16,9 @@ browser.find_element_by_xpath('//*[@class="cbot_area"]/a').click()
 html = browser.page_source
 soup = BeautifulSoup(html,'lxml')
 
-items = soup.find() 
-titles = soup.find_all('a')
+tables = soup.find("table",{"class":"tbl"})
+titles = tables.findAll("td",{"class":"left"})
 
 for title in titles:
-    print(title.get_text())
+    a_tag = title.find("a").text
+    print(a_tag)
