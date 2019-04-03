bd8c647930a99e0380025a8f05060fd5ddf38a5a
alsghdlah1 <alsghdlah1@naver.com>
2019-04-03 21:01:49 +0900

    3weeks

--- 3주차/최민호/lecture.py
+++ 3주차/최민호/lecture.py
@@ -1,7 +1,7 @@
 """
 {"name":"minho", "age": 26}           #key , value로 이루어짐 / ""는 문자열
 [{"name":"minho", "age": 26}]         #[]안에 item은 1개 들어있음.
-[{"name":"minho", "age": 26}]         #{}안에 item은 2개 들어있음.
+[{"name":"minho", "age": 26}]         #{}안에 item은 2개 들어있음. 
 
 #표 형태의 데이터를 json으로 표현하기
 [
@@ -12,7 +12,7 @@
 #tag 태그 <ul></ul> <a />
 #property 속성 class, id, href, title, src
 #property value 속성값 class="greet"에서 greet이 속성값
-#<a href="www.google.com">구글</a>
+#<a href="www.google.com">구글</a> 
 # => 태그는 <a>  속성은 href 속성값은 www.google.com
 
 """18강 네이버 API Token발급받기 """
@@ -20,8 +20,11 @@
 import os
 import sys
 import urllib.request
+import requests
 client_id = "UT1da6xBXa7fCjltgysw"
 client_secret = "lgopUhVpqo"
+#client_id = ""
+#client_secret = ""
 encText = urllib.parse.quote("IBK기업은행")
 url = "https://openapi.naver.com/v1/search/blog?query=" + encText # json 결과
 # url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
@@ -42,7 +45,7 @@ from urllib.parse import urlparse
 
 keyword = "방향제"
 url = "https://openapi.naver.com/v1/search/blog?query=" + keyword # json 결과
-result = requests.get(urlparse(url).geturl(),
+result = requests.get(urlparse(url).geturl(), 
 headers ={"X-Naver-Client-Id" :"",
           "X-Naver-Client-Secret":""})
 
@@ -53,7 +56,7 @@ print(json_obj)
 #import os
 
 #p = Path(__file__).parents[1] + '/최민호/hello.csv'
-#print(p)b
+#print(p)
 
 #path = 'C:/Users/Minho/Desktop/WebScrapping/StudyRepository/3주/최민호'
 #file = open(path + "/hello.csv", "w+")    #csv는 엑셀, w는 write
