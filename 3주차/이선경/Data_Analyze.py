3e66f3829afbc03854eed114208b41834b318893
swanilee <ssunkyung00@gmail.com>
2019-04-04 14:58:19 +0900

    lesson 3

---
+++ 3주차/이선경/Data_Analyze.py
@@ -0,0 +1,14 @@
+###Python Pandas로 데이터 분석하기
+###2000개의 검색결과에서 가장 많이 등장한 블로거 이름은?
+###- 어떤 블로거에게 광고를 의뢰하면 노출이 많이될까?
+###-우리의 키워드는 대연동 맛집! , 성공적으로 마케팅을 성공하기위해서는 필수적으로 알아보아야한다
+
+#pandas를 사용해 json파일을 불러오는것
+import pandas as pd
+
+temp = pd.read_json("")
+
+ #print(temp.count())
+#print(bloggernames)
+#temp_sum = temp.groupby("bloggername").count()
+#print(temp_sum)
---
+++ 3주차/이선경/MAKE_EXCEL.py
@@ -0,0 +1,16 @@
+#<part1>
+
+####웹에서 수집한 파일 엑셀로 저장하기
+
+# file = open("./hello.csv","w+") #파이썬 파일이 실행되는 위치
+####파일에 기록가능
+# file.write("hello, everyone" + "\n")
+# file.write("bye" + "\n")
+####csv파일이란 , comma separated value (콤마로 구분된 값)
+
+
+#<part2>
+
+####NAVER 검색 결과 100개씩 10페이지 가지고 오기!
+#-모듈로 분리 한 후에 여러번 호출하기
+
